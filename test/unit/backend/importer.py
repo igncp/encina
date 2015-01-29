e55353dc4c5ba8d496879d3b8c64ec42f444c663
Ignacio Carbajo <icarbajop@gmail.com>
2015-01-29 21:14:06 +0100

    Fix import issue

--- test/unit/backend/importer.py
+++ test/unit/backend/importer.py
@@ -2,10 +2,11 @@ import sys
 import os
 
 helpers_path = os.path.realpath(os.path.dirname(__file__) + '../../../helpers/python/')
-helpers_path = os.path.realpath(os.path.dirname(__file__) + '../../../fixtures/python/')
+fixtures_path = os.path.realpath(os.path.dirname(__file__) + '../../../fixtures/python/')
 src_path = os.path.realpath(os.path.dirname(__file__) + '../../../../src/python')
 
 sys.path.insert(0, helpers_path)
+sys.path.insert(0, fixtures_path)
 sys.path.insert(0, src_path)
 
 import encina_test_helpers.imports.common_modules as modules
